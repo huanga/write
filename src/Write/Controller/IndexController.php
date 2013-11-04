67696b0d954c69fe5e3a6c07e54c89f5c1a8d049
Andy Huang <andy.huang@terrandin.com>
2013-11-03 23:10:06 -0800

    Minor refactor to make controllers detect authentication status a bit better.

--- src/Write/Controller/IndexController.php
+++ src/Write/Controller/IndexController.php
@@ -3,26 +3,8 @@ namespace Write\Controller;
 
 use Write\Model\User;
 
-class IndexController extends AbstractController {
+class IndexController extends AuthRequiredController {
 	public function IndexAction() {
-		$authenticated = false;
 
-		if ( $this->session->has( 'username' ) ) {
-			$username = $this->session->get( 'username' );
-			$password = $this->session->get( 'password' );
-
-			if ( User::authenticate( $username, $password ) ) {
-				$authenticated = true;
-			}
-		}
-
-		if ( !$authenticated ) {
-			header( 'HTTP/1.1 301 Moved Permanently' );
-			header( 'Location: /write/session/new' );
-			$this->dispatcher->forward( array(
-				'controller' => 'session',
-				'action'     => 'new'
-			) );
-		}
 	}
 }
