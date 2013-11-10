1170b92cc2a5af3b5c4db65e9a633d5ddda21e8c
Andy Huang <andy.huang@terrandin.com>
2013-11-10 00:34:48 -0800

    Added stricter type hinting.

--- write/Write/Resources/VoltTemplateEngine.php
+++ write/Write/Resources/VoltTemplateEngine.php
@@ -4,7 +4,7 @@ namespace Write\Resources;
 use Phalcon\Mvc\View\Engine\Volt as PhalconVoltEngine;
 
 class VoltTemplateEngine extends AbstractResource {
-	public function __construct( $view, $di ) {
+	public function __construct( $view, DependencyInjector $di ) {
 		$this->_instance = new PhalconVoltEngine( $view, $di );
 		$this->_instance->setOptions(array(
 			'compiledPath' => './cache/volt/'
