9584b24b90b21a421f551b80d47cae4bc76b2229
Andy Huang <andy.huang@terrandin.com>
2013-11-11 01:32:02 -0800

    What the heck happened to the imports...

--- write/index.php
+++ write/index.php
@@ -1,18 +1,12 @@
 <?php
 use Phalcon\Exception as PhalconException;
-use Phalcon\Exception as PhalconException;
-use Write\Resources\Application;
 use Write\Resources\Application;
 use Write\Resources\DatabaseAdapter;
 use Write\Resources\DependencyInjector;
-use Write\Resources\DependencyInjector;
-use Write\Resources\Dispatcher;
 use Write\Resources\Dispatcher;
 use Write\Resources\Router;
 use Write\Resources\SessionManager;
 use Write\Resources\ViewManager;
-use Write\Resources\ViewManager;
-use Write\Resources\VoltTemplateEngine;
 use Write\Resources\VoltTemplateEngine;
 
 define('APPPATH', __DIR__);
