8553e12f2260659fb2955c558ba364044a996b57
root <root@quizrpg-qdb.andyhuang.net>
2013-12-14 01:57:01 -0800

    New searching algorithm and fixes on the padding.

--- write/assets/js/qdb.js
+++ write/assets/js/qdb.js
@@ -1,15 +1,18 @@
 // Setup Filtering Tool
 var qdb = new Array();
 $('#contents').find('ul > li').each(function() {
-    var answer = $(this).find('p').html();
-    if ( answer == undefined ) { answer = $(this).find('ol').html(); }
-    var entry = {
-        node: $(this).clone(),
-        question: $(this).find('h2').html(),
-        answer: answer
+    var question = $(this).find('h2').html();
+    if ( question.indexOf('Sample Question') != 0 ) { 
+        var answer   = $(this).find('p').html();
+        if ( answer == undefined ) { answer = $(this).find('ol').html(); }
+        var entry = {
+            node: $(this).clone(),
+            question: question,
+            answer: answer
+        }
+        $(this).remove();
+        qdb.push(entry);
     }
-    $(this).remove();
-    qdb.push(entry);
 });
 
 var options = {
@@ -17,7 +20,7 @@ var options = {
     id: 'node'
 }
 
-var f = new Fuse( qdb, options );
+// var f = new Fuse( qdb, options );
 
 // Helper Functions 
 function __qdb_add_dom( node ) {
@@ -41,12 +44,60 @@ function qdb_filter( keyword ) {
     } else {
         __qdb_hideall();
 
+        var aMatches = qdb.filter(function(item) {
+            var a = item.answer;
+            var k = 0; // remembers position of last found character in answer
+
+            // consider each search character one at a time
+            for (var i = 0; i < keyword.length; i++) {
+                var l = keyword[i];
+                if (l == ' ') continue;                        // ignore spaces
+                nk = a.indexOf(l, k+1);                        // search for character & update position in answer
+                if (nk == -1) return false;                    // if it's not found, exclude this item
+                if ((k != 0) && ((nk - k) > 5)) return false; // skip too far apart
+                k = nk;
+            }
+            return true;
+        });
+        var qMatches = qdb.filter(function(item) {
+            var q = item.question;  // Shortcut
+
+            var j = 0; // remembers position of last found character in question
+
+            // consider each search character one at a time
+            for (var i = 0; i < keyword.length; i++) {
+                var l = keyword[i];
+                if (l == ' ') continue;                        // ignore spaces
+                nj = q.indexOf(l, j+1);                        // search for character & update position in question
+                if (nj == -1) return false;                    // if it's not found, exclude this item
+                if ((j != 0) && ((nj - j) > 5)) return false; // skip too far apart
+                j = nj;
+            }
+            return true;
+        });
+        qMatches.forEach(function(match) {
+            __qdb_add_dom(match.node);
+        });
+        aMatches.forEach(function(match) {
+            __qdb_add_dom(match.node);
+        });
+    }
+}
+
+/*
+function qdb_filter( keyword ) {
+    if ( keyword == '' ) {
+        __qdb_showall();
+    } else {
+        __qdb_hideall();
+
         var results = f.search( keyword );
         $.each( results, function( index, result ) {
 		__qdb_add_dom( result );
         });
     }
 }
+*/
 
 // Bind Search UI
 $('#qdb_search').keyup(function() {
@@ -61,5 +112,21 @@ $('#searchform').submit(function( event ) {
 
 
 // Init page
-__qdb_hideall();
-__qdb_showall();
+$(document).ready(function () {
+    var isQDB = ( window.location.href.indexOf("/Entertainment") !== -1 );
+        isQDB = isQDB || ( window.location.href.indexOf("/Geography") !== -1 );
+        isQDB = isQDB || ( window.location.href.indexOf("/History") !== -1 );
+        isQDB = isQDB || ( window.location.href.indexOf("/Lifestyle") !== -1 );
+        isQDB = isQDB || ( window.location.href.indexOf("/Science") !== -1 );
+        isQDB = isQDB || ( window.location.href.indexOf("/Sports") !== -1 );
+        isQDB = isQDB || ( window.location.href.indexOf("/Sorting") !== -1 );
+
+    if (isQDB === true) {
+        // Get rid of place holder questions
+        __qdb_hideall();
+        __qdb_showall();
+    } else {
+        // Get rid of search bar
+        $('#searchform').hide();
+    }
+});
